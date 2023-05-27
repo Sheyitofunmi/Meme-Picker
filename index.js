c3b56c6e039069dc5419f26f1b5baedbb869d7fa
Ajewole seyi <99263653+Sheyitofunmi@users.noreply.github.com>
2023-05-27 20:32:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function closeModal(){
 
 function renderCat(){
     const catObject = getSingleCatObject()
-    cosole.log(catObject.image)
+    console.log(catObject.image)
     memeModalInner.innerHTML =  `
         <img 
         class="cat-img" 
